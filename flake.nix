8d0cb2e1361533447a54f36f6140bfb9b7f8e48e
Erdi ARI <me@erdiari.dev>
2024-07-17 14:13:06 +0300

    Changed home-manager name to erd. I use it by default.

--- flake.nix
+++ flake.nix
@@ -56,7 +56,7 @@
     # Standalone home-manager configuration entrypoint
     # Available through 'home-manager --flake .#your-username@your-hostname'
     homeConfigurations = {
-      default = home-manager.lib.homeManagerConfiguration {
+      erd = home-manager.lib.homeManagerConfiguration {
         pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
         extraSpecialArgs = {inherit inputs outputs unstable-pkgs;};
         # > Our main home-manager configuration file <
