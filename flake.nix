1b9954d4fdd08f24fd44d8e1f8df56149f566b0d
Erdi ARI <me@erdiari.dev>
2025-02-17 14:46:13 +0300

    Added cuda-maintainers cachix and changed waybar

--- flake.nix
+++ flake.nix
@@ -29,11 +29,12 @@
     in {
       nixConfig = {
         extra-substituters =
-          [ "https://nix-community.cachix.org" "https://hyprland.cachix.org" "https://devenv.cachix.org" ];
+          [ "https://nix-community.cachix.org" "https://hyprland.cachix.org" "https://devenv.cachix.org" "https://cuda-maintainers.cachix.org" ];
         extra-trusted-public-keys = [
           "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
           "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
           "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
+          "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
         ];
       };
       nixosConfigurations = {
