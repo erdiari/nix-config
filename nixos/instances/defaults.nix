2f49f2a8200d876d8126fc966fd2577be67d3980
Erdi ARI <me@erdiari.dev>
2025-04-30 15:54:38 +0300

    Changed display manager to sddm from gdm

--- nixos/instances/defaults.nix
+++ nixos/instances/defaults.nix
@@ -97,10 +97,11 @@
 
   # Enable the X11 windowing system.
   services.xserver.enable = true;
-  # services.displayManager.ly.enable = true;
+  services.displayManager = {enable=true;sddm.enable = true;};
+
+  # services.xserver.displayManager.gdm.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
   services.xserver.desktopManager.xfce.enable = true;
 
