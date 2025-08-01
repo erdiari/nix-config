96307a0a572f00737e2b3811f8f979ee485fd4f0
Erdi ARI <me@erdiari.dev>
2025-08-01 22:50:45 +0300

    Update flake.lock dependencies and stylix.nix

--- nixos/modules/stylix.nix
+++ nixos/modules/stylix.nix
@@ -23,10 +23,10 @@
       };
     };
 
-    # targets = {
-    #   firefox = {
-    #     profileNames = [ "default" ];
-    #   };
-    # };
+    targets = {
+      firefox = {
+        profileNames = [ "default" ];
+      };
+    };
   };
 }
